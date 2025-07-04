18dbbb02763054e8785398314c7e7da477893735
Sjain0018 <sj5373557@gmail.com>
2025-07-04 23:42:31 +0530

    Change ath in contact

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import ContactSection from '../components/courses-page/Contactsection'
+import ContactSection from '../components/contact-page/Contactsection'
 import WhatsappIcon from '../components/common/Whatsappicon'
 
 const Contact = () => {
